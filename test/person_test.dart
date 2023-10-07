1df02d83cf7bd13c6e3ab5d391cb19119288b6c6
Muhammad Farabie <farabie024@gmail.com>
2023-10-07 09:03:02 +0700

    unit testing

--- test/person_test.dart
+++ test/person_test.dart
@@ -2,9 +2,36 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_tutor/model/person.dart';
 
 void main() {
-  Person p;
-  p = Person();
-  test("Inisialisasi Object Person", () {
-    expect(p.name, "no name");
+  group("Mengecek Class Person", () {
+    Person p;
+    p = Person();
+    test("Inisialisasi Object Person", () {
+      print("Test 1");
+      expect(p.name, equals("no name"));
+      expect(p.age, equals(0));
+    });
+    test("agenya harus positif", () {
+      print("Test 2");
+      p.age = -5;
+      expect(p.age, isPositive);
+    });
+
+    test("Angka Keberuntungan harus 3 buah bilangan positif", () {
+      expect(p.luckyNumbers,
+          allOf([hasLength(equals(3)), isNot(anyElement(isNegative))]));
+    });
+  });
+
+  setUp(() {
+    //Setup ini digunakan untuk persiapan
+    //Misalnya untuk mempersiapkan database sebelum melakukan test
+    print("Set Up");
+  });
+
+  tearDown(() {
+    //Teardown ini menutup
+    //Misalnya jika sudah digunakan database nya baik itu testnya
+    //Berhasil atau tidak makan databasenya harus ditutup
+    print("Tear Down");
   });
 }
