219b3f1da6d15ad65ade6981c8f8d8c73188228f
Muhammad Farabie <farabie024@gmail.com>
2023-10-19 23:05:11 +0700

    Firebase Anonymous

---
+++ lib/ui/page/firebase_authanonymous.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class FirebaseAnonymous extends StatelessWidget {
+  const FirebaseAnonymous({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Firebase Authentication"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Dibedkan repository karena untuk firebas harus inisialisasi dulu di widget binddingnya",
+              ),
+              Text(
+                  "Untuk Materi ini cek repository dengan nama firebase_authanonymous"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
